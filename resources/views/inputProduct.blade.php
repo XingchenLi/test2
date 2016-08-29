eacf236b66f4e2d4d584c25a01ce6f140aa1bbdc
XingchenLi <lix17@miamioh.edu>
2016-08-29 22:51:07 +0800

    finishing the inputProduct view

--- resources/views/inputProduct.blade.php
+++ resources/views/inputProduct.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-    <div class="col-md-8">
+    <div class="col-md-12">
         <h3>输入产品页面</h3>
         <form action="#" method="post">
 
@@ -45,78 +45,194 @@
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="form-group">
                     <label for="title"> 成立日期</label>
                     <input class="form-control" type="text" name="title" id="title">
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="form-group">
                     <label for="title"> 最新规模</label>
                     <input class="form-control" type="text" name="title" id="title">
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="form-group">
                     <label for="title"> 管理经理人</label>
                     <input class="form-control" type="text" name="title" id="title">
                 </div>
             </div>
 
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="form-group">
                     <label for="title"> 累计单位净值</label>
                     <input class="form-control" type="text" name="title" id="title">
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
 
                 <div class="form-group">
                     <label for="title"> 累计分红</label>
                     <input class="form-control" type="text" name="title" id="title">
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="form-group">
                     <label for="title"> 基金经理</label>
                     <input class="form-control" type="text" name="title" id="title">
                 </div>
             </div>
-            <hr>
-            <hr>
-            <div class="form-group">
-                <label for="title">基金全称 </label>
-                <input class="form-control" type="text" name="title" id="title">
+
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title">基金全称 </label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="title"> 基金简称</label>
-                <input class="form-control" type="text" name="title" id="title">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金简称</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="title"> 基金代码</label>
-                <input class="form-control" type="text" name="title" id="title">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金代码</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="title"> 成立日期</label>
-                <input class="form-control" type="text" name="title" id="title">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 成立日期</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="title"> 上市日期</label>
-                <input class="form-control" type="text" name="title" id="title">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 上市日期</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="title"> 存续期限</label>
-                <input class="form-control" type="text" name="title" id="title">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 存续期限</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="title"> 上市地点</label>
-                <input class="form-control" type="text" name="title" id="title">
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 上市地点</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金总份额（亿份）</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 上市流通份额（亿份）</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金规模（亿元）</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 选股风格</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金管理人</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金托管人</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金经理</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 运作方式</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金类别</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 二级分类</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title">代销机构 </label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 最代参与金额</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 代赎回金额</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 投资目标</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 基金比较基准</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    <label for="title"> 投资范围</label>
+                    <input class="form-control" type="text" name="title" id="title">
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <label for="article"> 投资范围</label>
+                    <textarea name="article" class="form-control" id="article" placeholder="输入文章" rows="10"></textarea>
+                </div>
+                <button type="Submit" class="btn btn-primary">提交</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
             </div>
 
 
-            <button type="Submit" class="btn btn-primary">提交</button>
-            <input type="hidden" name="_token" value="{{ Session::token() }}">
+
 
         </form>
 
